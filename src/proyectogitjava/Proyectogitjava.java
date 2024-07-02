1389b72ef5a6358e42a762cbe913ed9b29e35c67
SilviaGV46 <lucenasil@hotmail.com>
2024-07-02 09:57:21 +0200

    FOTO1 ESTE ES MI PRIMER PROYECTO

--- src/proyectogitjava/Proyectogitjava.java
+++ src/proyectogitjava/Proyectogitjava.java
@@ -7,6 +7,8 @@ public class Proyectogitjava {
    
     public static void main(String[] args) {
         System.out.println("HOLA MUNDO");
+        
+          System.out.println("HOLA SILVIA");
     }
     
 }
